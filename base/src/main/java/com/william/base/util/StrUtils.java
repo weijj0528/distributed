29e42130d4dc9c81e72775bd7d7efc3cf114fd9c
weijj0528 <weijj0528@163.com>
2017-07-08 10:39:38 +0800

    添加手机与邮箱效验方法

--- base/src/main/java/com/william/base/util/StrUtils.java
+++ base/src/main/java/com/william/base/util/StrUtils.java
@@ -1,10 +1,22 @@
 package com.william.base.util;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by vijay on 2017/4/13.
  */
 public class StrUtils {
 
+    private static Pattern emailP = Pattern.compile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?");
 
+    private static Pattern phoneP = Pattern.compile("^1[3456789]\\d{9}$");
+
+    public static boolean isEmail(String email) {
+        return emailP.matcher(email).matches();
+    }
+
+    public static boolean isPhone(String email) {
+        return phoneP.matcher(email).matches();
+    }
 
 }
