f8b95d6bdbb67fd756d6146a0580d98d399f927a
weijj0528 <weijj0528@163.com>
2017-07-15 21:59:06 +0800

    字符串工具类调整

--- base/src/main/java/com/william/base/util/StrUtils.java
+++ base/src/main/java/com/william/base/util/StrUtils.java
@@ -1,5 +1,7 @@
 package com.william.base.util;
 
+import org.apache.commons.lang3.RandomUtils;
+
 import java.util.regex.Pattern;
 
 /**
@@ -11,12 +13,29 @@ public class StrUtils {
 
     private static Pattern phoneP = Pattern.compile("^1[3456789]\\d{9}$");
 
+    private static String[] Numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
     public static boolean isEmail(String email) {
         return emailP.matcher(email).matches();
     }
 
     public static boolean isPhone(String email) {
         return phoneP.matcher(email).matches();
+    }
+
+    public static String randomNumberStr(int length) {
+        StringBuffer str = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * 10);
+            str.append(Numbers[index]);
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.err.println(randomNumberStr(4));
+        }
     }
 
 }
